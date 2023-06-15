f22074059a27078545672014d71fbd65378d8ad2
ONE-I0 <davidjayvee22@gmail.com>
2023-06-16 03:53:54 +0800

    finish syntax,input,variables and adjust some other stuff with sounds too

--- app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaIntroduction.java
+++ app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaIntroduction.java
@@ -1,6 +1,7 @@
 package com.example.care_prototype_01.JavaLessonsActivity;
 
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -22,12 +23,14 @@ public class JavaIntroduction extends AppCompatActivity {
     TextView bullet_introduction4;
     TextView bullet_introduction5;
     TextView bullet_introduction6;
-
+    private MediaPlayer question_sounds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_introduction);
 
+        //declare for the sounds
+        question_sounds = MediaPlayer.create(JavaIntroduction.this, R.raw.question);
 
         bullet_introduction = findViewById(R.id.introduction_bullet);
         String platform = "•\tPlatform Independence: Java is a \"write once, run anywhere\" language. This means that once you write a Java program, you can run it on any platform that has a Java Virtual Machine (JVM). The JVM acts as an interpreter, allowing the Java code to be executed on different operating systems.";
@@ -96,44 +99,48 @@ public class JavaIntroduction extends AppCompatActivity {
     private void OpenAlertBox() {
         AlertDialog.Builder builder = new AlertDialog.Builder(JavaIntroduction.this);
         builder.setTitle("Codey's Trivia Sentence")
-                .setIcon(R.drawable.sample_codey)
+                .setIcon(R.drawable.codey_java_dyk)
                 .setMessage("Did you know that Java was originally named \"Oak\"? The language was initially developed by James Gosling and his team at Sun Microsystems in the early 1990s. However, they discovered that the name \"Oak\" was already trademarked, so they had to come up with a new name. They brainstormed various alternatives and eventually settled on \"Java\" inspired by their love for coffee. The name \"Java\" refers to the coffee that comes from the Indonesian island of Java. So, the programming language we know today as Java was named after a type of coffee!")
                 .setPositiveButton("Okay", (dialogInterface, i) -> {
                 });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        question_sounds.start();
     }
 
     private void openQuestion1() {
         AlertDialog.Builder builder = new AlertDialog.Builder(JavaIntroduction.this);
         builder.setTitle("Is Java a programming or scripting language?")
-                .setIcon(R.drawable.sample_codey)
+                .setIcon(R.drawable.codey_java_dyk)
                 .setMessage("Java is primarily considered a programming language rather than a scripting language. In Java, the source code is compiled into bytecode, which is then executed by a Java Virtual Machine (JVM). This compilation step helps to optimize performance and allows the code to be run on any platform with a compatible JVM.")
                 .setPositiveButton("Okay", (dialogInterface, i) -> {
                 });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        question_sounds.start();
     }
 
     private void openQuestion2() {
         AlertDialog.Builder builder = new AlertDialog.Builder(JavaIntroduction.this);
         builder.setTitle("What makes JAVA different to other languages?")
-                .setIcon(R.drawable.sample_codey)
+                .setIcon(R.drawable.codey_java_dyk)
                 .setMessage("Compared to other programming languages, Java is fast, reliable and secure. From desktop to web applications, scientific supercomputers to gaming consoles, cell phones to the Internet, Java is used in every nook and corner.")
                 .setPositiveButton("Okay", (dialogInterface, i) -> {
                 });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        question_sounds.start();
     }
     private void openQuestion3() {
         AlertDialog.Builder builder = new AlertDialog.Builder(JavaIntroduction.this);
         builder.setTitle("What makes Java so unique?")
-                .setIcon(R.drawable.sample_codey)
+                .setIcon(R.drawable.codey_java_dyk)
                 .setMessage("Because it is one of the main programming languages, Java has a vast and thriving developer community worldwide. The community contributes to the continuous growth and improvement of Java by sharing knowledge, developing frameworks, tools, and libraries, and providing support to fellow developers. The active community ensures that Java remains relevant and up-to-date with emerging trends and technologies. Thus, it is easy to learn Java and there are so many resources available that can be searched online.")
                 .setPositiveButton("Okay", (dialogInterface, i) -> {
                 });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        question_sounds.start();
     }
 }
 
--- app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaSyntax.java
+++ app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaSyntax.java
@@ -1,16 +1,138 @@
 package com.example.care_prototype_01.JavaLessonsActivity;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.care_prototype_01.R;
 
 public class JavaSyntax extends AppCompatActivity {
+    Button btn_DYK_1,btn_question1, btn_question2, btn_question3;
+    Button btn_ans_01;
 
+    TextView answer;
+    ImageView answer_1;
+    private MediaPlayer question_sounds;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_syntax);
+
+        //declare for the sounds
+        question_sounds = MediaPlayer.create(JavaSyntax.this, R.raw.question);
+
+
+        //declare R.id For the answer of syntax
+        answer = (TextView) findViewById(R.id.AnswerCodeyTextView);
+        answer_1 = (ImageView) findViewById(R.id.AnswerCodeyImage);
+
+        //this will open the answer for syntax
+        btn_ans_01 = findViewById(R.id.btnAnswer01);
+        btn_ans_01.setOnClickListener(view ->
+                openANS_1()
+        );
+        //this will open the did you know in syntax
+        btn_DYK_1 = findViewById(R.id.btdyk1);
+        btn_DYK_1.setOnClickListener(view ->
+                openDYK1()
+        );
+
+        //this will open the three questions
+        btn_question1 = findViewById(R.id.btnquestion1);
+        btn_question1.setOnClickListener(view ->
+                openQuestion1()
+        );
+
+        btn_question2 = findViewById(R.id.btnquestion2);
+        btn_question2.setOnClickListener(view ->
+                openQuestion2()
+        );
+
+        btn_question3 = findViewById(R.id.btnquestion3);
+        btn_question3.setOnClickListener(view ->
+                openQuestion3()
+        );
+
+    }
+
+
+    private void openDYK1() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(JavaSyntax.this);
+        builder.setTitle("Did you know?")
+                .setIcon(R.drawable.codey_java_dyk)
+                .setMessage("identifiers in Java are case-sensitive? It is because Java identifies any lowercase and uppercase letters as distinct characters. This means that even though there are two identical words written in the code, the program treats it as two separate identifiers in Java!")
+                .setPositiveButton("Okay", (dialogInterface, i) -> {
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        question_sounds.start();
+
+
+
+    }
+    private void openANS_1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(JavaSyntax.this);
+        builder.setTitle("Answer is!")
+                .setIcon(R.drawable.codey_java_dyk)
+                .setMessage("Output:\n" +
+                        "Hello World\n")
+                .setPositiveButton("Okay", (dialogInterface, i) -> {
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        question_sounds.start();
+
+        //this will let the invisible sentence and image for codey to visible
+        answer.setVisibility(View.VISIBLE);
+        answer_1.setVisibility(View.VISIBLE);
+    }
+
+
+    //program for the three questions
+    private void openQuestion1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(JavaSyntax.this);
+        builder.setTitle("What will happen if I declare two variables with same spelling but different values?")
+                .setIcon(R.drawable.codey_java_dyk)
+                .setMessage("The program will give an error to the output because it cannot determine which of the two is being called out to the execution of the code.")
+                .setPositiveButton("Okay", (dialogInterface, i) -> {
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        question_sounds.start();
+
+    }
+
+    private void openQuestion2() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(JavaSyntax.this);
+        builder.setTitle("Can I make a program without using an identifier?")
+                .setIcon(R.drawable.codey_java_dyk)
+                .setMessage("The answer is No. Even at the start of making a program, the first thing that you will see in the code is the main method. Removing it will cause an error to the whole code.")
+                .setPositiveButton("Okay", (dialogInterface, i) -> {
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        question_sounds.start();
+    }
+    private void openQuestion3() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(JavaSyntax.this);
+        builder.setTitle(" What is the difference of identifier to keyword?")
+                .setIcon(R.drawable.codey_java_dyk)
+                .setMessage("Keywords are reserved words that have a special meaning in the programming language. Therefore, using a keyword as an identifier is prohibited because it has a distinct meaning that can be interpreted by the compiler.")
+                .setPositiveButton("Okay", (dialogInterface, i) -> {
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        question_sounds.start();
     }
 }
