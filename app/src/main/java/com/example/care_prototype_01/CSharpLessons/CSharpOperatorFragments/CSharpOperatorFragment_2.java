90ae5e95f46ebbe05e92f2815b2ce43b00cf8b2a
ONE-I0 <davidjayvee22@gmail.com>
2023-07-19 02:37:38 +0800

    done with operators

--- app/src/main/java/com/example/care_prototype_01/CSharpLessons/CSharpOperatorFragments/CSharpOperatorFragment_2.java
+++ app/src/main/java/com/example/care_prototype_01/CSharpLessons/CSharpOperatorFragments/CSharpOperatorFragment_2.java
@@ -1,5 +1,8 @@
 package com.example.care_prototype_01.CSharpLessons.CSharpOperatorFragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +10,47 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.care_prototype_01.R;
 
 public class CSharpOperatorFragment_2 extends Fragment {
 
+
+    private MediaPlayer question_sounds;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_c_sharp_operator_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_c_sharp_operator_2, container, false);
+
+        //declare for the sounds
+        question_sounds = MediaPlayer.create(requireActivity(), R.raw.question);
+
+        Button btnShowAlertDialog = view.findViewById(R.id.python_operator_DYK1);
+        btnShowAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDYK1();
+            }
+        });
+
+        return view;
+    }
+
+    private void showDYK1() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setIcon(R.drawable.codey_c_cut);
+        alertDialogBuilder.setTitle("Did you know!");
+        alertDialogBuilder.setMessage("In C#, you may use a variety of operators to perform various operations on variables and values. ");
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // You can add some action here when the user clicks "OK"
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        question_sounds.start();
     }
 }
---
+++ app/src/main/java/com/example/care_prototype_01/CSharpLessons/CSharpOperatorFragments/CSharpOperatorFragment_3.java
@@ -0,0 +1,102 @@
+package com.example.care_prototype_01.CSharpLessons.CSharpOperatorFragments;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.care_prototype_01.R;
+
+
+public class CSharpOperatorFragment_3 extends Fragment {
+
+
+    private MediaPlayer question_sounds;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_c_sharp_operator_3, container, false);
+
+        //declare for the sounds
+        question_sounds = MediaPlayer.create(requireActivity(), R.raw.question);
+
+
+        Button btnP_Intro_Q1 = view.findViewById(R.id.P_Intro_btnQ1);
+        Button btnP_Intro_Q2 = view.findViewById(R.id.P_Intro_btnQ2);
+        Button btnP_Intro_Q3 = view.findViewById(R.id.P_Intro_btnQ3);
+        btnP_Intro_Q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnP_Intro_Q1();
+            }
+        });
+        btnP_Intro_Q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnP_Intro_Q2();
+            }
+        });
+        btnP_Intro_Q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnP_Intro_Q3();
+            }
+        });
+        return view;
+    }
+    private void btnP_Intro_Q1() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setIcon(R.drawable.codey_c_cut);
+        alertDialogBuilder.setTitle("What is the arithmetic operator used for multiplication in C#?");
+        alertDialogBuilder.setMessage("The arithmetic operator used for multiplication in C# is the asterisk (*) operator.");
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // You can add some action here when the user clicks "OK"
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        question_sounds.start();
+    }
+    private void btnP_Intro_Q2() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setIcon(R.drawable.codey_c_cut);
+        alertDialogBuilder.setTitle("What is the logical operator used to perform a logical OR operation in C#?");
+        alertDialogBuilder.setMessage("The logical operator used to perform a logical OR operation in C# is the double pipe (||) operator.");
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // You can add some action here when the user clicks "OK"
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        question_sounds.start();
+    }
+    private void btnP_Intro_Q3() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setIcon(R.drawable.codey_c_cut);
+        alertDialogBuilder.setTitle("What is the relational operator used to check if two values are not equal in C#?");
+        alertDialogBuilder.setMessage("The relational operator used to check if two values are not equal in C# is the exclamation mark followed by the equals sign (!=) operator.");
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // You can add some action here when the user clicks "OK"
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        question_sounds.start();
+    }
+}
