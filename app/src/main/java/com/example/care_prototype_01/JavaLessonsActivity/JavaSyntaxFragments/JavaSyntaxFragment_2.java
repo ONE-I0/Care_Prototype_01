0a706e289cf3c3e3cbb515524f05ccad7719472a
ONE-I0 <davidjayvee22@gmail.com>
2024-02-06 18:07:59 +0800

    finish app

--- app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaSyntaxFragments/JavaSyntaxFragment_2.java
+++ app/src/main/java/com/example/care_prototype_01/JavaLessonsActivity/JavaSyntaxFragments/JavaSyntaxFragment_2.java
@@ -22,43 +22,48 @@ public class JavaSyntaxFragment_2 extends Fragment {
         View view = inflater.inflate(R.layout.fragment_java_syntax_2, container, false);
 
         // Example syntax print
-        WebView codeWebView_J_Syntax_1_1= view.findViewById(R.id.java_code_syn1);
-        String formattedCode_J_Syntax_1_1 = getString(R.string.java_code_syn1);
+        WebView codeWebView_J_Syntax_1_1= view.findViewById(R.id.j_syn1);
+        String formattedCode_J_Syntax_1_1 = getString(R.string.java_syn1);
         String htmlContent_J_Syntax_1_1 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_1_1);
         configureWebView(codeWebView_J_Syntax_1_1, htmlContent_J_Syntax_1_1);
 
         // Example syntax print
-        WebView codeWebView_J_Syntax_2= view.findViewById(R.id.java_code_syn2);
-        String formattedCode_J_Syntax_2 = getString(R.string.java_code_syn2);
+        WebView codeWebView_J_Syntax_2= view.findViewById(R.id.j_syn2);
+        String formattedCode_J_Syntax_2 = getString(R.string.java_syn2);
         String htmlContent_J_Syntax_2 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_2);
         configureWebView(codeWebView_J_Syntax_2, htmlContent_J_Syntax_2);
 
-        WebView codeWebView_J_Syntax_3= view.findViewById(R.id.java_code_syn3);
-        String formattedCode_J_Syntax_3 = getString(R.string.java_code_syn3);
+        WebView codeWebView_J_Syntax_3= view.findViewById(R.id.j_syn3);
+        String formattedCode_J_Syntax_3 = getString(R.string.java_syn3);
         String htmlContent_J_Syntax_3 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_3);
         configureWebView(codeWebView_J_Syntax_3, htmlContent_J_Syntax_3);
 
         // Example syntax print
-        WebView codeWebView_J_Syntax_4= view.findViewById(R.id.java_code_syn4);
-        String formattedCode_J_Syntax_4 = getString(R.string.java_code_syn4);
+        WebView codeWebView_J_Syntax_4= view.findViewById(R.id.j_syn4);
+        String formattedCode_J_Syntax_4 = getString(R.string.java_syn4);
         String htmlContent_J_Syntax_4 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_4);
         configureWebView(codeWebView_J_Syntax_4, htmlContent_J_Syntax_4);
 
 
-        WebView codeWebView_J_Syntax_5= view.findViewById(R.id.java_code_syn5);
-        String formattedCode_J_Syntax_5 = getString(R.string.java_code_syn5);
+        WebView codeWebView_J_Syntax_5= view.findViewById(R.id.j_syn5);
+        String formattedCode_J_Syntax_5 = getString(R.string.java_syn5);
         String htmlContent_J_Syntax_5 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_5);
         configureWebView(codeWebView_J_Syntax_5, htmlContent_J_Syntax_5);
 
-        WebView codeWebView_J_Syntax_6= view.findViewById(R.id.java_code_syn6);
-        String formattedCode_J_Syntax_6 = getString(R.string.java_code_syn6);
+        WebView codeWebView_J_Syntax_6= view.findViewById(R.id.j_syn6);
+        String formattedCode_J_Syntax_6 = getString(R.string.java_syn6);
         String htmlContent_J_Syntax_6 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_6);
         configureWebView(codeWebView_J_Syntax_6, htmlContent_J_Syntax_6);
 
-        WebView codeWebView_J_Syntax_7= view.findViewById(R.id.java_code_syn7);
-        String formattedCode_J_Syntax_7 = getString(R.string.java_code_syn7);
+        WebView codeWebView_J_Syntax_7= view.findViewById(R.id.j_syn7);
+        String formattedCode_J_Syntax_7 = getString(R.string.java_syn7);
         String htmlContent_J_Syntax_7 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_7);
         configureWebView(codeWebView_J_Syntax_7, htmlContent_J_Syntax_7);
+
+        WebView codeWebView_J_Syntax_8= view.findViewById(R.id.j_syn8);
+        String formattedCode_J_Syntax_8 = getString(R.string.java_syn8);
+        String htmlContent_J_Syntax_8 = String.format("<html><body style=\"font-size: %dpx;\">%s</body></html>", dpToPx(), formattedCode_J_Syntax_8);
+        configureWebView(codeWebView_J_Syntax_8, htmlContent_J_Syntax_8);
         return view;
     }
 
--- app/src/main/java/com/example/care_prototype_01/SplashScreen.java
+++ app/src/main/java/com/example/care_prototype_01/SplashScreen.java
@@ -2,25 +2,36 @@ package com.example.care_prototype_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
+// SplashScreen class extending AppCompatActivity
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the layout for the splash screen
         setContentView(R.layout.splash_screen);
 
+        // Create a handler to delay the start of the main activity
         Handler handler = new Handler();
+
+        // Use the postDelayed method to delay the runnable execution
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                // Create an intent to start the main activity
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+
+                // Start the main activity
+                startActivity(intent);
+
+                // Finish the splash screen activity to prevent it from being accessible via the back button
+                finish();
             }
-        },4000);
+        }, 4000); // 4000 milliseconds (4 seconds) delay before starting the main activity
     }
 }
